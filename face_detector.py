679fcfd60de2c8ac5aa34ae63ccb3c16de75794a
AnvitaManne <anvita.manne@gmail.com>
2025-10-20 16:33:10 +0530

    added image detection

--- face_detector.py
+++ face_detector.py
@@ -1,70 +1,87 @@
 import cv2
-
 from random import randrange
+import random
+
 #load some pre-trained data on face frontals from opencv(hear cascade algorithm)
 trained_face_data=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 
-#to capture a video to detect face in
-#can put video instead of 0
-webcam=cv2.VideoCapture(0)
+#choose mode
+mode = input("Enter 'webcam' to use webcam or 'image' to detect in an image: ").strip().lower()
 
-#iterate forever over frames
-while True:
-    #read the current _read,frame=webcam.read()
+if mode == 'webcam':
+    #to capture a video to detect face in
+    #can put video instead of 0
+    webcam=cv2.VideoCapture(0)
 
-    #must convert frame to grayscale
-    grayscaled_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #iterate forever over frames
+    while True:
+        #read the current frame
+        success, frame = webcam.read()
+        if not success:
+            break
+
+        #must convert frame to grayscale
+        grayscaled_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+        #detect faces
+        face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
+
+        #draw rectangles around
+        for(x,y,w,h) in face_coordinates:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(randrange(256),randrange(256),randrange(256)),5)
+            
+        cv2.imshow('Clever Programmer Face Detector',frame)
+        #it will wait 1ms and automatically press a key
+        key=cv2.waitKey(10)
+
+        #stop if Q key is pressed
+        #ascii for q Q
+        if key==81 or key==113:
+            break
+
+    #release the video capture object
+    webcam.release()
+    cv2.destroyAllWindows()
+
+elif mode == 'image':
+    #choose an image to detect face in
+    #import an img
+    img_path = input("Enter the full path to the image: ").strip()
+    # Remove any surrounding quotes
+    img_path = img_path.strip('"').strip("'")
+    # Convert backslashes to forward slashes
+    img_path = img_path.replace("\\", "/")
+
+    img = cv2.imread(img_path)
+    if img is None:
+        print("Error: Image not found. Check the path.")
+        exit()
+
+
+    #to convert to black n white
+    grayscaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     #detect faces
     face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
 
-    #draw rectangles around
-    for(x,y,w,h) in face_coordinates:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(randrange(256),randrange(256),randrange(256)),5)
-        
-    cv2.imshow('Clever Programmer Face Detector',frame)
-    #it will wait 1ms and automatically press a key
-    key=cv2.waitKey(10)
+    #draw rect around the face
+    #(top-left point i.e x, y,width,height)
+    cv2.rectangle(img,(68,24),(68+54,24+54),(0,255,0),2)
 
-    #stop if Q key is pressed
-    #ascii for q Q
-    if key==81 or key==113:
-        break
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(random.randrange(256),random.randrange(256),random.randrange(256)),5)
 
-#release the video capture object
-webcam.release()
+    #naming the window tht will pop up and displaying img
+    cv2.imshow('Clever Programmer Face Detector',img)
+     
+    #to keep image open till a key is pressed
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    print('code completed')
+
+else:
+    print("Invalid input. Please enter 'webcam' or 'image'.")
 
 
 
- 
-'''
-import random
-#load some pre-trained data on face frontals from opencv(hear cascade algorithm)
-trained_face_data=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 
-#choose an image to detect face in
-#import an img
-img=cv2.imread("C:\\Users\\91636\\OneDrive\\Desktop\\rdjr.png")
-
-#to convert to black n white
-grayscaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-#detect faces
-face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
-
-#draw rect around the face
-#(top-left point i.e x, y,width,height)
-cv2.rectangle(img,(68,24),(68+54,24+54),(0,255,0),2)
-
-for (x,y,w,h) in face_coordinates:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(random.randrange(256),random.randrange(256),random.randrange(256)),5)
-
-#print(face_coordinates)
-
-#naming the window tht will pop up and displaying img
-cv2.imshow('Clever Programmer Face Detector',img)
- 
-#to keep image open till a key is pressed
-cv2.waitKey()
-print('code completed')
-'''
